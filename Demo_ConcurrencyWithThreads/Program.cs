f09f5ea98becdb820a998d3d73c0a5ccedce69af
Thomas Hedegaard <thomashj93@gmail.com>
2020-08-22 07:48:16 +0200

    add simple thread demo for how to initialize a thread

--- Demo_ConcurrencyWithThreads/Program.cs
+++ Demo_ConcurrencyWithThreads/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Demo_ConcurrencyWithThreads
 {
@@ -6,7 +8,40 @@ namespace Demo_ConcurrencyWithThreads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Enter to start threading...");
+            Console.ReadLine();
+
+            // initialize new thread
+            Thread t = new Thread(PrintOne);
+
+            Console.WriteLine("Before thread start...");
+            // start executing thread t
+            t.Start();
+            Console.WriteLine("After thread start...");
+            PrintZero();
+
+            Console.ReadLine();
         }
+         
+        public static void PrintOne()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(1);
+            }
+            Console.WriteLine();
+            Console.WriteLine("PrintOne done");
+        }
+
+        public static void PrintZero()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(0);
+            }
+            Console.WriteLine();
+            Console.WriteLine("PrintZero done");
+        }
+
     }
 }
