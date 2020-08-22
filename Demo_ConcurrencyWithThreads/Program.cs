404924ce478ed686624a59e6d38f41ff416ccd0b
Thomas Hedegaard <thomashj93@gmail.com>
2020-08-22 07:18:19 +0200

    init setup

---
+++ Demo_ConcurrencyWithThreads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo_ConcurrencyWithThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
